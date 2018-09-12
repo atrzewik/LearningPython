66903a4bf8c28511d95723eaac0df9a909dcdcca
atrzewik <42387362+atrzewik@users.noreply.github.com>
2018-09-12 20:40:48 +0200

    make it finally objective

--- CircleAndCross.py
+++ CircleAndCross.py
@@ -4,10 +4,11 @@ from time import sleep
 class Board(object):
 
     def __init__(self, number_of_fields):
-        fields = []
+        self.number_of_fields = number_of_fields
+        self.capacity = self.calculate_capacity()
+        self.fields = []
         for i in range(number_of_fields):
-            fields.append(["-"]*number_of_fields)
-        self.fields = fields
+            self.fields.append(["-"] * number_of_fields)
 
     def __str__(self):
         new_board = ""
@@ -15,137 +16,141 @@ class Board(object):
             new_board += " ".join(row) + "\n"
         return new_board
 
-    def co_ordinates_of_signs(self, number_of_fields, sign, field):
+    def calculate_capacity(self):
+        return self.number_of_fields * self.number_of_fields
+
+    def co_ordinates_of_signs(self, sign):
         list_of_signs = []
-        for i in range(number_of_fields):
-            for j in range(number_of_fields):
-                if field[i][j] == sign:
+        for i in range(self.number_of_fields):
+            for j in range(self.number_of_fields):
+                if self.fields[i][j] == sign:
                     list_of_signs.append([i, j])
         return list_of_signs
 
-    def return_true_or_false(self, number_of_fields, winning, sign, field):
-        list_of_signs = self.co_ordinates_of_signs(number_of_fields, sign, field)
+    def mark_field(self, row, column, sign):
+        if self.fields[int(row)][int(column)] != "-":
+            raise AttributeError("This field %d, %d is busy." % (row, column))
+        else:
+            self.fields[int(row)][int(column)] = sign
+
+    def check_winning_condition(self, winning, sign):
+        list_of_signs = self.co_ordinates_of_signs(sign)
         for i in range(len(list_of_signs)):
             for j in range(2):
                 count = 0
                 for sign in list_of_signs:
                     if sign in winning:
                         count += 1
-                if count == number_of_fields:
+                if count == self.number_of_fields:
                     return True
                 else:
                     return False
 
-    def row_winning_condition(self, number_of_fields, sign, field):
+    def row_winning_condition(self, sign):
         winning = []
         x, y = 0, 0
-        for i in range(number_of_fields):
+        for i in range(self.number_of_fields):
             winning.append([x, y + i])
-        return self.return_true_or_false(number_of_fields, winning, sign, field)
+        return self.check_winning_condition(winning, sign)
 
-    def column_winning_condition(self, number_of_fields, sign, field):
+    def column_winning_condition(self, sign):
         winning = []
         x, y = 0, 0
-        for i in range(number_of_fields):
+        for i in range(self.number_of_fields):
             winning.append([x + i, y])
-        return self.return_true_or_false(number_of_fields, winning, sign, field)
+        return self.check_winning_condition(winning, sign)
 
-    def diagonal_top_condition(self, number_of_fields, sign, field):
+    def diagonal_top_condition(self, sign):
         winning = []
         x, y = 0, 0
-        for i in range(number_of_fields):
-            winning.append([x + i, y + (number_of_fields - 1) - i])
-        return self.return_true_or_false(number_of_fields, winning, sign, field)
+        for i in range(self.number_of_fields):
+            winning.append([x + i, y + (self.number_of_fields - 1) - i])
+        return self.check_winning_condition(winning, sign)
 
-    def diagonal_down_condition(self, number_of_fields, sign, field):
+    def diagonal_down_condition(self, sign):
         winning = []
         x, y = 0, 0
-        for i in range(number_of_fields):
+        for i in range(self.number_of_fields):
             winning.append([x + i, y + i])
-        return self.return_true_or_false(number_of_fields, winning, sign, field)
+        return self.check_winning_condition(winning, sign)
 
-    def check_the_winner(self, number_of_fields, sign, field):
-        if self.row_winning_condition(number_of_fields, sign, field) or self.column_winning_condition(number_of_fields, sign, field) or self.diagonal_top_condition(number_of_fields, sign, field) or self.diagonal_down_condition(number_of_fields, sign, field):
-            return True
-        else:
-            return False
+    def check_the_winner(self, sign):
+        return self.row_winning_condition(sign) or self.column_winning_condition(sign) or self.diagonal_top_condition(
+            sign) or self.diagonal_down_condition(sign)
 
 
 class Game(object):
 
     def __init__(self):
-        answer = True
+        self.turn = 0
         print("Hello!", "\n ...")
-        sleep(1)
-        while answer:
-            number_of_fields = int(input("Please pick number of fields from 2 to 20: "))
-            self.fields = Board(number_of_fields).fields
-            self.board = Board(number_of_fields)
+        self.board = Board(self.get_number_of_fields())
+        while True:
+            print("Turn: ", self.turn)
             print(self.board)
-            maximum_pick = number_of_fields - 1
-            if number_of_fields < 2 or number_of_fields > 20:
-                print("Wrong number!")
-                answer = True
-            else:
-                for i in range(self.board_capacity(number_of_fields)):
-                    moving = True
-                    while moving:
-                        sign = self.whose_move(i)
-                        print("Turn: ", self.turn_number(i))
-                        row = input("Please, enter your row(0-%s) for %s:" % (maximum_pick, sign))
-                        column = input("Please, enter your column(0-%s) for %s:" % (maximum_pick, sign))
-                        moving = self.make_move(row, column, maximum_pick)
-                        if moving:
-                            continue
-                        else:
-                            self.fields[int(row)][int(column)] = sign
-                            field = self.fields
-                            if self.continue_game(number_of_fields, sign, i, field):
-                                print(self.board)
-                            else:
-                                if self.turn_number(i) == self.board_capacity(number_of_fields):
-                                    print(self.board)
-                                    print("Remis!")
-                                elif Board(number_of_fields).check_the_winner(number_of_fields, sign, field):
-                                    print(self.board)
-                                    print("%s win!" % sign)
-                                    break
-                answer = False
+            self.make_move()
+            if not self.continue_game():
+                print(self.board)
+                break
+            self.turn += 1
 
-    def board_capacity(self, number_of_fields):
-        capacity = number_of_fields * number_of_fields
-        return capacity
-
-    def turn_number(self, turn):
-        turns = 1
-        turns += turn
-        return turns
-
-    def make_move(self, row, column, maximum_pick):
-        answer = True
-        if (int(row) or int(column)) > maximum_pick or (int(row) or int(column)) < 0:
-            print("You pick wrong answer! Try again", "\n", self.board)
-        elif self.fields[int(row)][int(column)] != "-":
-            print("Occupaied!, Try again", "\n", self.board)
+    def get_number_of_fields(self):
+        number = int(input("Please pick number of fields from 2 to 20: "))
+        try:
+            if number < 2 or number > 20:
+                raise AttributeError
+        except AttributeError:
+            print("You pick wrong number of fields!")
+            self.get_number_of_fields()
         else:
-            answer = False
-        return answer
+            return number
 
-    def continue_game(self, number_of_fields, sign, turns, field):
-        if self.board.check_the_winner(number_of_fields, sign, field):
+    def whose_move(self):
+        if self.turn % 2 == 0:
+            return "o"
+        else:
+            return "x"
+
+    def make_move(self):
+        sign = self.whose_move()
+        maximum_pick = self.board.number_of_fields - 1
+        row = input("Please, enter your row(0-%s) for %s:" % (maximum_pick, sign))
+        column = input("Please, enter your column(0-%s) for %s:" % (maximum_pick, sign))
+        try:
+            self.board.mark_field(row, column, sign)
+        except AttributeError:
+            print(AttributeError)
+            print("You pick wrong answer! Try again")
+            self.make_move()
+
+    def continue_game(self):
+        sign = self.whose_move()
+        if self.board.check_the_winner(sign):
+            print("The winner is %s" % sign)
             return False
-        elif self.turn_number(turns) == self.board_capacity(number_of_fields):
+        elif self.turn == self.board.capacity:
+            print("Remis!")
             return False
         else:
             return True
 
-    def whose_move(self, turns):
-        turn = self.turn_number(turns)
-        if turn % 2 != 0:
-            sign = "o"
-        else:
-            sign = "x"
-        return sign
+
+# print(Game())
+
+def test_winning_condition_row():
+    number_of_fields = 3
+    board = Board(number_of_fields)
+    board.mark_field(0, 0, 'O')
+    board.mark_field(0, 1, 'O')
+    board.mark_field(0, 2, 'O')
+    print("wywololuje sie?>")
+    assert board.check_the_winner('O')
 
 
+# board = Board(5)
+# board.mark_field(0,0,"o")
+# board.mark_field(2,3,"x")
+# board.mark_field(0,0,"x")
+# print (board)
+test_winning_condition_row()
 print(Game())
